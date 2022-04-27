4c1b57d514737a7a91fb1571286dad5b14f4d395
Lucas Caldeira <mrrealcaldeira@gmail.com>
2022-04-27 12:16:24 -0300

    Componente: Input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Button, Flex, Stack } from '@chakra-ui/react';
+import { Input } from '../components/Form/input';
 
-export default function Home() {
+export default function SignIn() {
   return (
-   <h1>Dash</h1>
+   <Flex 
+    w='100vw' 
+    h='100vh' 
+    align='center' 
+    justify='center'
+  >
+     <Flex 
+      as='form'
+      width="100%"
+      maxWidth={360}
+      bg="gray.800"
+      p="8"
+      borderRadius={8}
+      flexDir="column"
+     >
+       <Stack spacing='4'>
+         <Input type='email' name="email" label='E-mail' />
+
+         <Input type='password' name="password" label='Senha' />
+       </Stack>
+
+       <Button type='submit' mt='6' colorScheme='pink' size='lg'>Entrar</Button>
+     </Flex>
+   </Flex>
   )
 }
