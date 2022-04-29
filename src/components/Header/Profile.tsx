1f9f3a7f21fbe7c6ed911a307a4f487eb886e1a0
Lucas Caldeira <mrrealcaldeira@gmail.com>
2022-04-29 14:16:34 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps){
   return(
     <Flex align='center'>
-    <Box mr='4' textAlign='right'>
+    { showProfileData && (
+      <Box mr='4' textAlign='right'>
       <Text>Lucas Caldeira</Text>
       <Text
         color='gray.300'
         fontSize='small'
       >mrrealcaldeira@gmail.com</Text>
     </Box>
+    )}
 
     <Avatar size='md' name="Lucas Caldeira" src='https://github.com/realcaldeira.png' />
   </Flex>
